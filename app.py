763251111ade02a80468285a8f5268d2a9a976ae
Av1352 <66872073+Av1352@users.noreply.github.com>
2023-03-27 16:51:51 +0530

    edho

--- app.py
+++ app.py
@@ -9,12 +9,16 @@ import capture as hand
 import preprocess as preprocess
 from predict import predict
 
+
 def capture_image():
     hand.capture()
     # img = cv2.imread("user.png")
 
+
 def preprocess_image():
+    preprocess.roi_hand()
     preprocess.preprocess_images()
+
 
 def predict_sign():
     prediction = predict()
@@ -24,10 +28,12 @@ def predict_sign():
     sign = prediction
     print(sign)
 
+
 def main():
     capture_image()
     preprocess_image()
     predict_sign()
+
 
 if __name__ == '__main__':
     main()
--- preprocess.py
+++ preprocess.py
@@ -3,21 +3,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import cv2
-import imageio
+import imageio.v2 as imageio
+import mediapipe as mp
+
+mphands = mp.solutions.hands
+hands = mphands.Hands()
+handCascade = mp.solutions.drawing_utils
+
+def roi_hand():
+    image = 'user.png'
+    # imagergb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    img = imageio.imread(image)
+    result = hands.process(img)
+    hand_landmarks = result.multi_hand_landmarks
+    h, w, c = img.shape
+    if hand_landmarks:
+        for handLMs in hand_landmarks:
+            x_max = 0
+            y_max = 0
+            x_min = w
+            y_min = h
+            for lm in handLMs.landmark:
+                x, y = int(lm.x * w), int(lm.y * h)
+                if x > x_max:
+                    x_max = x
+                if x < x_min:
+                    x_min = x
+                if y > y_max:
+                    y_max = y
+                if y < y_min:
+                    y_min = y
+            rect = cv2.rectangle(img, (x_min, y_min),
+                (x_max, y_max), (255, 255, 255), 2)
+            roi = img[y_min:y_max, x_min: x_max]
+        cv2.imwrite(filename='roi.png', img=roi)
 
 # Preprocessing all the images to extract ROI i.e. hands
-
 def preprocess_images():
-    image = 'user.png'
+    image = 'roi.png'
     #reading image
     img=imageio.imread(image)
     #Converting image to grayscale
     gray_img=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
     #Converting image to HSV format
     hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    cv2.imshow("hsv",hsv_img)
     #Defining boundary level for skin color in HSV
     skin_color_lower= np.array([0,40,30],np.uint8)
-    skin_color_upper= np.array([43,255,255],np.uint8)
+    skin_color_upper= np.array([96,255,255],np.uint8)
     #Producing mask
     skin_mask=cv2.inRange(hsv_img,skin_color_lower,skin_color_upper)
     #Removing Noise from mask
@@ -37,4 +70,5 @@ def preprocess_images():
     # return canny
 
 if __name__ == '__main__':
+    roi_hand()
     preprocess_images()
